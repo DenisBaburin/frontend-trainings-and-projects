43900c410f2e00bac38c1d7a2184867e6b35acbf
DenisBaburin <denis.v.baburin@gmail.com>
2023-09-03 22:45:10 +1100

    continue education

--- src/components/screens/home/Home.jsx
+++ src/components/screens/home/Home.jsx
@@ -1,12 +1,16 @@
-import {cars} from './cars.data.js'
+import {cars as carsData} from './cars.data.js'
 import CarItem from './car-item/CarItem'
 import CreateCarForm from './create-car-form/CreateCarForm.jsx'
+import { useState } from 'react'
 
 function Home() {
+    const [cars, setCars] = useState(carsData)
+    console.log(cars)
+    
     return (
       <div>
         <h1>Cars catalog</h1>
-        <CreateCarForm /> 
+        <CreateCarForm setCars={setCars}/> 
         <div>
          {cars.length ? ( 
             cars.map(car => <CarItem key={car.id} car={car}/>)
--- src/components/screens/home/create-car-form/CreateCarForm.jsx
+++ src/components/screens/home/create-car-form/CreateCarForm.jsx
@@ -1,20 +1,35 @@
 import { useState } from 'react'
 import styles from './CreateCarForm.module.css'
 
-const CreateCarForm = () => {
+const CreateCarForm = ({setCars}) => {
     const [name, setName] = useState('')
+    const [price, setPrice] = useState('')
+    const [image, setImage] = useState('')
 
-    const createCar = () => {
-        console.log(name)
+    const createCar = e => {
+        e.preventDefault()
+        setCars(prev => [...prev, {id: prev.length + 1, name, price, image}])
     }
 
     return (
         <form className={styles.form}>
-            <input placeholder='Name'/>
-            <input placeholder='Price'/>
-            <input placeholder='Image'/>
+            <input
+                 placeholder='Name'
+                 onChange={e => setName (e.target.value)}
+                 value={name}
+            />
+            <input 
+                 placeholder='Price'
+                 onChange={e => setPrice (e.target.value)}
+                 value={price}
+            />
+            <input 
+                 placeholder='Image'
+                 onChange={e => setImage (e.target.value)}
+                 value={image}            
+            />
 
-            <button>Create</button>
+            <button className='btn' onClick={e => createCar(e)}>Create</button>
         </form>
     )
 }
